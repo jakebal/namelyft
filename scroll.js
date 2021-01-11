c74c168a0ccc99a8cf3ca500f3d0395610ebf34b
jakebal <jacobebalcom@gmail.com>
2021-01-10 20:25:55 -0500

    a

--- scroll.js
+++ scroll.js
@@ -1,15 +1,17 @@
 
 var navbar = document.getElementById("navbar-wrapper");
-
+var contactUs = document.getElementById("contact-us-nav-link");
 var scroll = 0;
-window.addEventListener("scroll", (event) => { 
+window.addEventListener("scroll", function(event) { 
     scroll = this.scrollY;
     if(scroll > 10)
     {
-        navbar.classList.add("navbar-white");
+        navbar.classList.add("navbar-scrolled");
+        contactUs.classList.add("contact-us-nav-link-scrolled");
     }
     else
     {
-        navbar.classList.remove("navbar-white");
+        navbar.classList.remove("navbar-scrolled");
+        contactUs.classList.remove("contact-us-nav-link-scrolled");
     }
 });
